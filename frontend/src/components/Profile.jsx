23c8b221ce976c11624d7dcbf9853b359b4672f1
RiyaAgarwal0101 <riya.agar0101@gmail.com>
2025-01-10 23:51:00 +0530

    url updated

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -30,7 +30,7 @@ const Profile = () => {
 
   const handleFollowToggle = async () => {
     try {
-      const res = await axios.post(`http://localhost:8000/api/v1/user/followorunfollow/${userProfile._id}`, {}, { withCredentials: true });
+      const res = await axios.post(`https://socialhouse.onrender.com/api/v1/user/followorunfollow/${userProfile._id}`, {}, { withCredentials: true });
       // Update local state based on response
      
 
